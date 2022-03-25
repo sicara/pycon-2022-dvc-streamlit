d032575037bb04433b7e5d19dcc970d31f703218
AntoineT <antoinet@sicara.com>
2022-03-25 09:22:35 +0100

    Add whoiam info

--- pages/introduction/whoiam.py
+++ pages/introduction/whoiam.py
@@ -3,3 +3,10 @@ import streamlit as st
 from constants import AUTHOR
 
 st.write(f"My name is {AUTHOR}")
+st.write("French, 🇫🇷 🥖")
+st.write("34 years old, Father of two daughters")
+st.write("Data Scientist 🧪, Head of Science at Sicara (add logo)")
+st.write("Mostly Computer Vision")
+
+st.write("Add a nice picture of me")
+st.write("Add contact info: email, github, linkedin, twitter")
