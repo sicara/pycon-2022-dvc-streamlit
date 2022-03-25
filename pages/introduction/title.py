7fcb4e04d6e25c9cd620057454fee945e32349b1
AntoineT <antoinet@sicara.com>
2022-03-25 09:15:34 +0100

    Make title slide

--- pages/introduction/title.py
+++ pages/introduction/title.py
@@ -1,14 +1,44 @@
 import streamlit as st
 
-from constants import TITLE
-from utils.html_factory import CSSStyle, make_div, st_write_bs4
+import constants
+from utils.html_factory import CSSStyle, make_div, make_img, st_write_bs4
 
+# Title
 big_title_style = CSSStyle(
     text_align="center",
-    font_size="58px",
-    margin="80px 150px 80px 150px",
-    font_weight="bold",
+    font_size="64px",
+    line_height="64px",
+    margin="40px 80px 60px 80px",
+    font_weight=700,
 )
-big_title = make_div(style=big_title_style, text=TITLE)
+big_title = make_div(style=big_title_style, text=constants.TITLE)
 
 st_write_bs4(big_title)
+
+
+# Author
+author_style = CSSStyle(
+    text_align="center",
+    font_size="48px",
+    line_height="48px",
+    font_style="italic",
+    margin_bottom="40px",
+)
+author = make_div(style=author_style, text=constants.AUTHOR)
+
+st_write_bs4(author)
+
+
+# Conference
+st_write_bs4(
+    make_img(
+        style=CSSStyle(
+            text_align="center",
+            width="80%",
+            margin_left="auto",
+            margin_right="auto",
+            display="block",
+        ),
+        src=constants.CONFERENCE_FULL_LOGO_PATH,
+    ),
+)
