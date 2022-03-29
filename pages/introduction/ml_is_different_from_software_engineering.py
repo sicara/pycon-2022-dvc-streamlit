47ccaa499aa9297a16be10a759974cd1852784f9
AntoineT <antoinet@sicara.com>
2022-03-29 18:41:33 +0200

    Update the intro

--- pages/introduction/ml_is_different_from_software_engineering.py
+++ pages/introduction/ml_is_different_from_software_engineering.py
@@ -1,3 +1,10 @@
-from utils.commands import st_command
+import streamlit as st
 
-st_command(label="Enter a command :)", initial_command="dvc status src/dvc.yaml")
+st.markdown(
+    """
+### Why ML is different from classical Software engineering
+- need to track the code **and the data** (training data, models, parameters, ...)
+- the output of a training process is not **predictable**: it is exploratory work: need to track experiments
+- need interactive UI to dig in model performance: test the model, show metrics graphs, ...
+    """
+)
